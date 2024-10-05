37d3c414e0248f9f08a0274153786dd77dc77331
sh3ld0 <driverac@unsa.edu.pe>
2024-10-04 19:53:58 -0500

    intentamos usar el flipV para invertir el rey boca abajo, nos dimos cuenta que no lo invierte

--- ejemplo.c
+++ ejemplo.c
@@ -2,13 +2,7 @@
 #include "figures.h"
 
 void display(){
-  char** white = whiteSquare;
-  char** gris = reverse(whiteSquare);
-  char** result = join(white, gris);
-  
-
-  char** casillas = repeatH(result,4);
-  interpreter(result);
-
-  interpreter(reverse(casillas));
+  char** rey = king;
+  char** espejoV = flipV(rey);
+  interpreter(espejoV);
 }
