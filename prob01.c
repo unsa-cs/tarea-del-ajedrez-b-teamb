2cced6e613995aaebd32a1dc4d63a2ebbaa4a1f5
MKFIVENINE <brayandeport@gmail.com>
2024-10-04 19:30:58 -0500

    Se puso codigo con la resolucion del prob01 que se obtuvo en la anterior sesion con el ejecutable

---
+++ prob01.c
@@ -0,0 +1,9 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+	char** white = whiteSquare;
+	char** grey = reverse(whiteSquare);
+ 	char** result = join(white, grey);
+	interpreter(result);
+}
