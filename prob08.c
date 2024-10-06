e38378dd2d21a89afa2072170d7323bc3234843a
MarianaVLJ <mluisj@unsa.edu.pe>
2024-10-06 12:26:26 -0500

    se crearon arreglos almacenando los casilleros para cada fila, luego con join se juntaron todos los casileros en una variable se mostró una fila

---
+++ prob08.c
@@ -0,0 +1,36 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** grey = reverse(whiteSquare);
+  char** row1[] = {whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey};
+  char** row2[] = {grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare};
+  char** row3[] = {whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey};
+  char** row4[] = {grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare};
+  char** row5[] = {whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey};
+  char** row6[] = {grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare};
+  char** row7[] = {whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey};
+  char** row8[] = {grey, whiteSquare, grey, whiteSquare, grey, whiteSquare, grey, whiteSquare};
+  
+  char** row1f = row1[0];
+  char** row2f = row2[0];
+  char** row3f = row3[0];
+  char** row4f = row4[0];
+  char** row5f = row5[0];
+  char** row6f = row6[0];
+  char** row7f = row7[0];
+  char** row8f = row8[0];
+
+  for(int i = 1; i < 8; i++){
+    row1f = join(row1f, row1[i]);
+    row2f = join(row2f, row2[i]);  
+    row3f = join(row3f, row3[i]);
+    row4f = join(row4f, row4[i]);
+    row5f = join(row5f, row5[i]);
+    row6f = join(row6f, row6[i]);
+    row7f = join(row7f, row7[i]);
+    row8f = join(row8f, row8[i]);
+  }  
+  interpreter(row5f);
+} 
+
