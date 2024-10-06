903de162e2cba376981b10a45829a7c424041cb3
zrempz <rmontanezp@unsa.edu.pe>
2024-10-06 10:08:45 -0500

    Con el codigo del ejercicio 4, se agrego la segunda fila utilizando la misma logica

---
+++ prob06.c
@@ -0,0 +1,23 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** greySquare = reverse(whiteSquare);
+  char** filaCasillas[2] = {whiteSquare, greySquare};
+  char** figuras[8] = {rook, knight, bishop, queen, king, bishop, knight, rook};
+  char** row1 = superImpose(figuras[0], filaCasillas[1]);
+  char** row2 = superImpose(pawn, filaCasillas[0]);
+
+  for(int i = 1; i < 8; i++){
+    int j = (i % 2 == 0) ? 1 : 0;
+    int k = (i % 2 == 0) ? 0 : 1;
+
+    char** tmp = superImpose(figuras[i], filaCasillas[j]);
+    char** tmpPawn = superImpose(pawn, filaCasillas[k]);
+
+    row1 = join(row1, tmp);
+    row2 = join(row2, tmpPawn);
+  }
+
+  interpreter(row2);
+}
