13cf458864d80dd1445da00917e8faef578e2da8
Santherss <rcolomay@unsa.edu.pe>
2024-10-05 20:36:53 -0500

    se creo una segunda columna con la inversa de la primera, con la funcion join se juntaron ambas columnas y se utilizo repeatH para repetir 4 veces, con esto obteniendo otra solucion

--- prob03.c
+++ prob03.c
@@ -3,9 +3,11 @@
 
 void display(){
   char** grey = reverse(whiteSquare);
-  char** two = up(grey, whiteSquare);
+  char** two = up(grey, whiteSquare); 
   char** col1 = repeatV(two, 2);
-  char** result = repeatH(col1, 8);
+  char** col2 = reverse(col1);
+  char** twoCols = join(col1, col2);
+  char** result = repeatH(twoCols, 4);
 
   interpreter(result);
 }
