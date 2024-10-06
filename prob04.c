7755e04048fa97705e2103001473d81400cf01b6
zrempz <rmontanezp@unsa.edu.pe>
2024-10-05 22:18:14 -0500

    Se optimizo el codigo del ejercicio 4 utilizando el operador ternario reemplazando la sentencia if else

--- prob04.c
+++ prob04.c
@@ -7,14 +7,9 @@ void display(){
   char** figuras[8] = {rook, knight, bishop, queen, king, bishop, knight, rook};
   char** result = superImpose(figuras[0], filaCasillas[1]);
   for(int i = 1; i < 8; i++){
-    if(i % 2 == 0){
-      char** tmp = superImpose(figuras[i], filaCasillas[1]);
-      result = join(result, tmp);
-    }
-    else{
-      char** tmp = superImpose(figuras[i], filaCasillas[0]);
-      result = join(result, tmp);
-    }
+    int j = (i % 2 == 0) ? 1 : 0;
+    char** tmp = superImpose(figuras[i], filaCasillas[j]);
+    result = join(result, tmp);
   }
   interpreter(result);
 }
