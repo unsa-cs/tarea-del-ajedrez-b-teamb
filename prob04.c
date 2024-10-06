fb44bd3637d67173337b70619fe6b39b732467e1
sh3ld0 <driverac@unsa.edu.pe>
2024-10-05 21:56:25 -0500

    utilizamos arreglos, para almacenar casilleros y figuras, luego un for donde iteramos en los arreglos y utilizandolos como argumentos para la funcion superImpose y lo almacenamos en otro arreglo y se probo el casillero 2 para ver si lo almacenaba correctamente

--- prob04.c
+++ prob04.c
@@ -3,20 +3,16 @@
 
 void display(){
   char** greySquare = reverse(whiteSquare);
-  char** rook1 = superImpose(rook, greySquare);
-  char** knight1 = superImpose(knight, whiteSquare);
-  char** bishop1 = superImpose(bishop, greySquare);
-  char** queen1 = superImpose(queen, whiteSquare);
-  char** king1 = superImpose(king, greySquare);
-  char** bishop2 = superImpose(bishop, whiteSquare);
-  char** knight2 = superImpose(knight, greySquare);
-  char** rook2 = superImpose(rook, whiteSquare);
-  char** result1 = join(rook1, knight1);
-  char** result2 = join(bishop1, queen1);
-  char** result3 = join(king1, bishop2);
-  char** result4 = join(knight2, rook2);
-  char** mid1 = join(result1, result2);
-  char** mid2 = join(result3, result4);
-  char** resultF = join(mid1, mid2);
-  interpreter(resultF);
+  char** filaCasillas[2] = {whiteSquare, greySquare};
+  char** figuras[8] = {rook, knight, bishop, queen, king, bishop, knight, rook};
+  char** final[8];
+  for(int i = 0; i < 8; i++){
+    if(i % 2 == 0){
+      final[i] = superImpose(figuras[i], filaCasillas[1]);
+    }
+    else{
+      final[i] = superImpose(figuras[i], filaCasillas[0]); 
+    }
+  }
+  interpreter(final[1]);
 }
