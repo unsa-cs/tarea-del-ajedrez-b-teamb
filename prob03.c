9e90aec241b7c0019cce59cee73c1eb0c1927462
Santherss <rcolomay@unsa.edu.pe>
2024-10-05 20:31:09 -0500

    se corrigio el argumento de 4 a 2 y utilizo repeatH para mostrar las 8 columnas, pero los colores no se intercalaron en las columnas

--- prob03.c
+++ prob03.c
@@ -4,8 +4,9 @@
 void display(){
   char** grey = reverse(whiteSquare);
   char** two = up(grey, whiteSquare);
-  char** col1 = repeatV(two, 4);
+  char** col1 = repeatV(two, 2);
+  char** result = repeatH(col1, 8);
 
-  interpreter(col1);
+  interpreter(result);
 }
 
