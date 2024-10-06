7905a43caf02949f49f3076711aa3ccc3d4a091e
zrempz <rmontanezp@unsa.edu.pe>
2024-10-05 22:27:39 -0500

    Se modifico el operador ternario, para que empieze con un casillero blanco, y luego utilizar la funcion reverse para obtener el resultado del ejercicio 5. Pero no funcionó, porque faltó modificar la primera casilla

--- prob05.c
+++ prob05.c
@@ -7,7 +7,7 @@ void display(){
   char** figuras[8] = {rook, knight, bishop, queen, king, bishop, knight, rook};
   char** result = superImpose(figuras[0], filaCasillas[1]);
   for(int i = 1; i < 8; i++){
-    int j = (i % 2 == 0) ? 1 : 0;
+    int j = (i % 2 == 0) ? 0 : 1;
     char** tmp = superImpose(figuras[i], filaCasillas[j]);
     result = join(result, tmp);
   }
