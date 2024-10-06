a56e56060b324a296a2dd56042ed2f4e1bff9236
zrempz <rmontanezp@unsa.edu.pe>
2024-10-06 13:25:52 -0500

    Se implemento una nueva idea mediante un bucle for para obtener la primera fila correctamente

--- prob07.c
+++ prob07.c
@@ -2,17 +2,23 @@
 #include "figures.h"
 
 void display(){
-  char** grey = reverse(whiteSquare);
-  char** knight1 = superImpose(rotateR(knight), grey);
-  char** knight2 = superImpose(rotateL(knight), whiteSquare);
-  char** chessBoard[] = {knight1, knight2};
-  char** result = chessBoard[0];
-  for(int i = 1; i < 2; i++){
-    result = join(result, chessBoard[i]);
+  char** greySquare = reverse(whiteSquare);
+  char** squares[2] = {whiteSquare, greySquare};
+  char** rowResult;
+
+  for(int i = 0; i < 1; i++){
+    int k = (i % 2 == 0) ? 1 : 0;
+    char** row = squares[k];
+
+    for(int j = 1; j < 8; j++){
+      if(k + 1 == 1){
+        k++;
+      }else{
+        k = 0;
+      }
+      row = join(row, squares[k]);
+    }
+    rowResult = row;
   }
-  char** row2 = flipV(result);
-  result = up(result, row2);
-  interpreter(result);
+  interpreter(rowResult);
 }
-
-
