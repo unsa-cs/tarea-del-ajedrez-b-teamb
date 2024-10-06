db37dd4363efc688fef943d79943876249a4795e
Santherss <rcolomay@unsa.edu.pe>
2024-10-05 20:25:54 -0500

    se modifico el codigo para resolver mediante la repeticion de columnas(repeatV), esperando una columna de 4 casillas y se mostro 8

--- prob03.c
+++ prob03.c
@@ -3,11 +3,9 @@
 
 void display(){
   char** grey = reverse(whiteSquare);
-  char** two = join(grey, whiteSquare);
-  char** row1 = repeatH(two, 4);
-  char** row2 = reverse(row1);
-  char** rows = up(row1, row2);
-  char** result = repeatV(rows, 2);
-  interpreter(result);
+  char** two = up(grey, whiteSquare);
+  char** col1 = repeatV(two, 4);
+
+  interpreter(col1);
 }
 
