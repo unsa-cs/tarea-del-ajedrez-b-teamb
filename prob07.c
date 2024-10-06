07e236fe22b12df0ac37c8853635acb47b500dbe
zrempz <rmontanezp@unsa.edu.pe>
2024-10-06 13:52:01 -0500

    Se implemento condicionales para manejar las coordenadas (almacenadas en arreglos), donde deberian estar los caballos, sobrepuestas sobre un casillero.

--- prob07.c
+++ prob07.c
@@ -6,18 +6,39 @@ void display(){
   char** squares[2] = {whiteSquare, greySquare};
   char** rowResult;
 
+  char** knight1 = rotateL(knight);
+  char** knight2 = rotateR(knight);
+  char** knight3 = knight;
+  char** knight4 = rotateL(rotateL(knight));
+
+  int posX[4] = {2, 3, 5, 7};
+  int posY[4] = {1, 3, 4, 6};
+  char** knights[4] = {knight1, knight2, knight3, knight4};
+  int itknight = 0;
+
   for(int i = 0; i < 8; i++){
-    int k = (i % 2 == 0) ? 1 : 0;
-    char** row = squares[k];
+    int itcolor = (i % 2 == 0) ? 1 : 0;
+    char** row = squares[itcolor];
 
     for(int j = 1; j < 8; j++){
-      if(k + 1 == 1){
-        k++;
+      if(itcolor + 1 == 1){
+        itcolor++;
       }else{
-        k = 0;
+        itcolor = 0;
       }
-      row = join(row, squares[k]);
+
+      char** col = squares[itcolor];
+
+      if( itknight < 4){
+        if(i == posX[itknight] && j == posY[itknight]){
+          col = superImpose(knights[itknight], col);
+          itknight++;
+        }
+      }
+
+      row = join(row, col);
     }
+
     if(i == 0){
       rowResult = row;
     }else{
