851714bff7e6ab1d6a4d987d74e815bd0c7a5b7a
MarianaVLJ <mluisj@unsa.edu.pe>
2024-10-05 21:17:42 -0500

    Usando nuevamente la función superImpose se obtuvo la segunda casilla esta con el caballo blanco encima de whiteSquare, siguiendo esta idea se implemento el resto de las figuras superpuestas en las casillas correspondientes

--- prob04.c
+++ prob04.c
@@ -4,5 +4,12 @@
 void display(){
   char** greySquare = reverse(whiteSquare);
   char** rook1 = superImpose(rook, greySquare);
-  interpreter(rook1);
+  char** knight1 = superImpose(knight, whiteSquare);
+  char** bishop1 = superImpose(bishop, greySquare);
+  char** queen1 = superImpose(queen, whiteSquare);
+  char** king1 = superImpose(king, greySquare);
+  char** bishop2 = superImpose(bishop, whiteSquare);
+  char** knight2 = superImpose(knight, greySquare);
+  char** rook2 = superImpose(rook, whiteSquare);
+  interpreter(knight1);
 }
