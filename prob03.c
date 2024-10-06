68bb67d21e86d208f3e28317f2c356cdb95bbfdb
MKFIVENINE <brayandeport@gmail.com>
2024-10-05 20:08:15 -0500

    Se uso la funcion repeatV para repetir 2 veces el anterior resultado

--- prob03.c
+++ prob03.c
@@ -3,10 +3,11 @@
 
 void display(){
   char** grey = reverse(whiteSquare);
-  char** result = join(grey, whiteSquare);
-  char** row1 = repeatH(result, 4);
+  char** two = join(grey, whiteSquare);
+  char** row1 = repeatH(two, 4);
   char** row2 = reverse(row1);
   char** rows = up(row1, row2);
-  interpreter(rows);
+  char** result = repeatV(rows, 2);
+  interpreter(result);
 }
 
