c37207fbf39b24e40ad813a75c266a5ccac4667e
sh3ld0 <driverac@unsa.edu.pe>
2024-10-06 11:16:13 -0500

    se redujo el tablero a un 2x2 y un caballo rotado en cada casillero

--- prob07.c
+++ prob07.c
@@ -5,10 +5,10 @@ void display(){
   char** grey = reverse(whiteSquare);
   char** knight1 = superImpose(rotateR(knight), grey);
   char** knight2 = superImpose(rotateL(knight), whiteSquare);
-  char** tablero[] = {knight1, whiteSquare, knight2, whiteSquare};
-  char** result = tablero[0];
-  for(int i = 1; i < 4; i++){
-    result = join(result, tablero[i]);
+  char** chessBoard[] = {knight1, knight2};
+  char** result = chessBoard[0];
+  for(int i = 1; i < 2; i++){
+    result = join(result, chessBoard[i]);
   }
   char** row2 = flipV(result);
   result = up(result, row2);
